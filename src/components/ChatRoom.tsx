e8bf941959a37d14410730aba8279bb10a2e4071
Rony Philip <ronyphilip751@gmail.com>
2023-10-23 03:15:15 +0000

    add context provider

--- src/components/ChatRoom.tsx
+++ src/components/ChatRoom.tsx
@@ -4,12 +4,16 @@ import Header from "./Header";
 import Messages from "./Messages";
 import MessageInput from "./MessageInput";
 
+import { ContextProvider } from "@/context/MessageInputContext";
+
 function ChatRoom() {
   return (
     <main className="flex flex-col h-screen w-screen">
       <Header />
-      <Messages />
-      <MessageInput />
+      <ContextProvider>
+        <Messages />
+        <MessageInput />
+      </ContextProvider>
     </main>
   )
 }
