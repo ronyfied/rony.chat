86660c4e9154aa6e51bbcaba1045db84bae73726
Rony Philip <ronyphilip751@gmail.com>
2023-10-23 03:17:48 +0000

    smaller icon

--- src/components/Reply.tsx
+++ src/components/Reply.tsx
@@ -1,10 +1,13 @@
 import { Tooltip } from "react-tooltip";
+import { useContext } from "@/context/MessageInputContext";
 
-function Reply() {
+function Reply({ displayName }: { displayName: string }) {
+  const { messageInputValue, updateMessageInputValue } = useContext();
+
   return (
-    <main className="cursor-pointer" data-tooltip-content="Mention this user" data-tooltip-id="reply" data-tooltip-delay-show={1500}>
-      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M9.33333 6.40001V0L0 11.2L9.33333 22.4V15.84C16 15.84 20.6667 18.4 24 24C22.6667 16 18.6667 8 9.33333 6.40001Z" fill="#ACACAC"/>
+    <main onClick={() => updateMessageInputValue(`${messageInputValue} @${displayName}`)} className="cursor-pointer" data-tooltip-content="Mention this user" data-tooltip-id="reply" data-tooltip-delay-show={1500}>
+      <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M7.77778 5.33334V0L0 9.33337L7.77778 18.6666V13.2C13.3333 13.2 17.2222 15.3334 20 20C18.8889 13.3334 15.5556 6.66666 7.77778 5.33334Z" fill="#ACACAC"/>
       </svg>
       <Tooltip id="reply" />
     </main>
